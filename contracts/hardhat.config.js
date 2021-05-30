f382b82aa31417c3c7a09eebe5a0dde0411a717a
Pablo Palomo <ppalomov@gmail.com>
2021-05-30 17:33:49 +0200

    Update hardhat.config.js

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 // This is a sample Hardhat task. To learn how to create your own go to
